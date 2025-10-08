ab86d3a00f51f366e96e92b34890712fbfd6b7e8
AngeloC <geloycabangal10@gmail.com>
2025-10-09 04:27:34 +0800

    Connect to DigitalOcean Managed MySQL

--- backend/database.js
+++ backend/database.js
@@ -8,15 +8,16 @@ let db;
 export async function connectDB() {
   try {
     db = await mysql.createPool({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'Cabangal10',
+      host: process.env.DB_HOST || 'df-mysql-whiskerwatch-do-user-27125628-0.f.db.ondigitalocean.com',
+      user: process.env.DB_USER || 'doadmin',
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME || 'whiskerwatch',
+      port: parseInt(process.env.DB_PORT) || 25060,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
       ssl: {
-        ca: fs.readFileSync(new URL('./ca.pem', import.meta.url))
+        ca: process.env.DB_CA_CERT  // Path to or content of ca-certificate.crt
       }
     });
     console.log('Connected to MySQL database!');
