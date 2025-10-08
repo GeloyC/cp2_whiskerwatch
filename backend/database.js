f6f2f5ac981546802bed56c1a256bbb55fd92903
AngeloC <geloycabangal10@gmail.com>
2025-10-09 04:59:25 +0800

    self sself-signed certificate

--- backend/database.js
+++ backend/database.js
@@ -7,19 +7,16 @@ dotenv.config();
 let db;
 export async function connectDB() {
   try {
-    db = await mysql.createPool({
-      host: process.env.DB_HOST || 'df-mysql-whiskerwatch-do-user-27125628-0.f.db.ondigitalocean.com',
-      user: process.env.DB_USER || 'doadmin',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME || 'whiskerwatch',
-      port: parseInt(process.env.DB_PORT) || 25060,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-      ssl: {
-        ca: process.env.DB_CA_CERT  // Path to or content of ca-certificate.crt
-      }
-    });
+    db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 25060,
+    ssl: {
+      rejectUnauthorized: false, //  Allow self-signed certificate
+    },
+});
     console.log('Connected to MySQL database!');
   } catch (err) {
     console.error('Failed to connect to MySQL:', err);
