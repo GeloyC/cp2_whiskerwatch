abe81613da170f387b3db959a88fee80c5cc6fcf
AngeloC <geloycabangal10@gmail.com>
2025-10-09 04:36:51 +0800

    fixed url in sessioncontext

--- frontend/src/context/SessionContext.jsx
+++ frontend/src/context/SessionContext.jsx
@@ -6,6 +6,8 @@ const SessionContext = createContext();
 export const useSession = () => useContext(SessionContext);
 
 export const SessionProvider = ({ children }) => {
+    const url = `https://cp2-whiskerwatch.onrender.com`;
+
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [notifications, setNotifications] = useState([]);
@@ -15,7 +17,7 @@ export const SessionProvider = ({ children }) => {
 
     const refreshSession = async () => {
         try {
-            const res = await axios.get("http://localhost:5000/user/api/session", {
+            const res = await axios.get(`${url}/user/api/session`, {
                 withCredentials: true,
             });
             setUser(res.data.user || null);
@@ -31,7 +33,7 @@ export const SessionProvider = ({ children }) => {
     };
 
     const logout = async () => {
-        try { await axios.post( "http://localhost:5000/user/logout",
+        try { await axios.post( `${url}/user/logout`,
                 {},
                 { withCredentials: true }
             );
@@ -46,7 +48,7 @@ export const SessionProvider = ({ children }) => {
     const fetchNotifications = async (user_id) => {
         if (!user_id) return;
         try {
-            const response = await axios.get(`http://localhost:5000/user/notifications/${user_id}`);
+            const response = await axios.get(`${url}/user/notifications/${user_id}`);
             setNotifications(response.data);
         } catch (err) {
             console.error('Failed to fetch notifications:', err);
