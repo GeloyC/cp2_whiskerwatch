16639a68a1661882af98d7410ca469bf1b2158d6
AngeloC <geloycabangal10@gmail.com>
2025-10-09 01:58:41 +0800

    vite.config.js modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: process.env.VITE_BASE_PATH || "./",
+  base: process.env.VITE_BASE_PATH || "/",
 
 });
