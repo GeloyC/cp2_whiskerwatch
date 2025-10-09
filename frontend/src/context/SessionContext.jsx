cda970361ffa321202788b197b6ec4cc2bcfe866
AngeloC <geloycabangal10@gmail.com>
2025-10-10 00:31:25 +0800

    remove redundant useNavigate()

--- frontend/src/context/SessionContext.jsx
+++ frontend/src/context/SessionContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"; 
 
 
 const SessionContext = createContext();
