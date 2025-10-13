698fd20ec42ebd689b972e5eeb045e4ba7ce6f27
AngeloC <geloycabangal10@gmail.com>
2025-10-14 02:41:07 +0800

    Fix: Browser cookie setHeader error

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -430,7 +430,7 @@ UserRoute.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Only secure in production
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      sameSite: "Lax",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000
     });
