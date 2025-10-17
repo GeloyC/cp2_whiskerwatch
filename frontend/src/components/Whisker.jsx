836fef60bf874cea317c8fc5c980140f893ad209
AngeloC <geloycabangal10@gmail.com>
2025-10-17 22:57:21 +0800

    asdasdasda

--- frontend/src/components/Whisker.jsx
+++ frontend/src/components/Whisker.jsx
@@ -142,7 +142,7 @@ const Whisker = () => {
                 <img src="/assets/icons/arrow-right-no-tail.png" alt="arrow to leaderboard" className='w-full h-full object-cover'/>
               </div>
             </Link>
-            <div>
+            <div className='flex flex-row h-full'>
               <div className={`flex relative min-w-3 h-60 bg-[#FFF] border-4 border-[#FFF] rounded-full overflow-hidden ${whiskerVisible ? 'h-0 hidden' : 'min-h-auto flex'} shadow-lg`}>
                 <div className='flex flex-col items-center justify-evenly h-full'>
                   <div className='w-[15px] object-fit'>
