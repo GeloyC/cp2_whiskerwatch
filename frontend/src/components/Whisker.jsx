af7348b82c25c943f41cba72930fe5282966d469
AngeloC <geloycabangal10@gmail.com>
2025-10-17 23:29:50 +0800

    WhiskerMeter UI 4

--- frontend/src/components/Whisker.jsx
+++ frontend/src/components/Whisker.jsx
@@ -136,9 +136,9 @@ const Whisker = () => {
           </div>
         ) : (
           <div className='flex flex-col gap-2 h-auto w-auto'>
-            <Link to="/leaderboard" className={`${whiskerVisible ? "h-0 hidden" : "min-h-aut flex"} w-full justify-between items-center bg-[#889132] hover:bg-[#C2CB6A] active:bg-[#889132] rounded-[10px] px-3 py-1`}>
-              <span className='text-[#FFF] text-[12px] whitespace-nowrap font-bold'>WhiskerMeter Leaderboards</span>
-              <div className='size-3 object-cover flex items-center justify-center'>
+            <Link to="/leaderboard" className={`${whiskerVisible ? "h-0 hidden" : "min-h-aut flex"} w-fit justify-end gap-2 items-center bg-[#B5C04A] hover:bg-[#C2CB6A] active:bg-[#889132] rounded-[10px] px-3 py-1`}>
+              <span className='text-[#FFF] text-[12px] whitespace-nowrap font-bold'>Leaderboards</span>
+              <div className='size-2 object-cover flex items-center justify-center'>
                 <img src="/assets/icons/arrow-right-no-tail.png" alt="arrow to leaderboard" className='w-full h-full object-cover'/>
               </div>
             </Link>
