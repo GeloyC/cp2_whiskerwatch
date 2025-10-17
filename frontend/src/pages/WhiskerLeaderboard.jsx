8b16132940b6d0fdba9884e49842a7762b917aef
AngeloC <geloycabangal10@gmail.com>
2025-10-18 00:37:55 +0800

    asdasdad

--- frontend/src/pages/WhiskerLeaderboard.jsx
+++ frontend/src/pages/WhiskerLeaderboard.jsx
@@ -32,14 +32,14 @@ const WhiskerLeaderboard = () => {
             />
 
             <NavigationBar />
-                <div className='flex flex-col items-center justify-start gap-2 h-full p-10'> 
-                    <div className='flex flex-col items-center w-[1000px] p-5 bg-[#FFFCF6] rounded-[10px] gap-4'>
+                <div className='flex flex-col items-center justify-start gap-2 h-full xl:p-10 lg:p-10 md:p-5'> 
+                    <div className='flex flex-col items-center xl:w-[1000px] lg:w-[1000px] md:w-[800px] w-full h-full xl:h-fit lg:h-fit md:h-fit p-5 bg-[#FFFCF6] rounded-[10px] gap-4'>
                         <div className='flex items-center justify-between w-full'>
                             <div className='size-15'>
                                 <img src="/assets/icons/medal.png" alt="" />
                             </div>
 
-                            <label className='text-3xl text-[#889132] font-bold'>
+                            <label className='text-3xl text-center text-[#889132] font-bold'>
                                 WHISKMETER LEADERBOARD
                             </label>
 
@@ -48,19 +48,19 @@ const WhiskerLeaderboard = () => {
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-2 w-full items-start py-3'>
+                        <div className='flex flex-col gap-2 w-full min-h-full items-start py-3'>
                             <table className='flex flex-col w-full'>
                                 <thead>
-                                    <tr className='grid grid-cols-4 w-full place-items-start p-2'>
+                                    <tr className='grid grid-cols-[15%_30%_30%_25%] w-full place-items-start p-2'>
                                         <th>Rank</th>
                                         <th>Name</th>
                                         <th>Badge</th>
                                         <th>Points</th>
                                     </tr>
                                 </thead>
-                                <tbody className='max-h-[400px] overflow-y-scroll scrollbar-thin flex flex-col gap-1'>
+                                <tbody className='xl:max-h-[400px] lg:max-h-[400px] md:max-h-[400px] overflow-y-scroll scrollbar-thin flex flex-col gap-1'>
                                     {whiskerLeaders.map((leaders, index) => (
-                                        <tr className='grid grid-cols-4 w-full place-items-center justify-items-start p-2 rounded-[5px] bg-[#F9F7DC] border-1 border-dashed border-[#8f8f8f]'>
+                                        <tr className='grid grid-cols-[15%_30%_30%_25%] w-full place-items-center justify-items-start p-2 rounded-[5px] bg-[#F9F7DC] border-1 border-dashed border-[#8f8f8f]'>
                                             <td className='font-bold'>{index + 1}</td>
                                             <td>{`${leaders.firstname} ${leaders.lastname}`}</td>
                                             <td>{leaders.badge}</td>
