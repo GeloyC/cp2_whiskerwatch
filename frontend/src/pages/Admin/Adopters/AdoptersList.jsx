eb3bbe663bee9cc9214b45d6bee7c0a8fbfbf656
AngeloC <geloycabangal10@gmail.com>
2025-10-21 06:42:13 +0800

    display cert on block

--- frontend/src/pages/Admin/Adopters/AdoptersList.jsx
+++ frontend/src/pages/Admin/Adopters/AdoptersList.jsx
@@ -294,16 +294,32 @@ const AdoptersList = () => {
                     {/* CERTIFICATE */}
                     {selectedAdoptee && (
                       <div className='absolute inset-0 flex flex-col items-center justify-center p-10 bg-black/50 bg-opacity-75'>
-                        {!certificateUrl ? (
-                          <div id="certificate-block" className='relative flex flex-col items-center bg-[#FFF] bg-[url(/assets/AdoptionCertificate/Signed_Adoption_Certificate.png)] bg-cover bg-center w-[1020px] h-[650px]'>
-                            <label className='absolute top-73 text-4xl font-bold'>{selectedAdoptee.cat_name}</label>
-                            <label className='absolute top-94 right-40 text-xl font-bold'>{selectedAdoptee.adopter}</label>
-                            <label className='absolute top-102 right-140 text-xl font-bold'>{selectedAdoptee.adoption_date}</label>
+                        {certificateUrl ? (
+                          <div className="relative flex flex-col items-center">
+                            <img
+                              src={certificateUrl}
+                              alt="Uploaded Certificate"
+                              className="rounded-xl shadow-lg w-[1020px] h-[650px] object-cover"
+                            />
+                            <p className="mt-4 text-green-700 font-semibold">
+                              This adoption already has a certificate.
+                            </p>
                           </div>
 
                         ) : (
-                          <div id="certificate-block" className='relative flex flex-col items-center bg-[#FFF] bg-[url(/assets/AdoptionCertificate/Signed_Adoption_Certificate.png)] bg-cover bg-center w-[1020px] h-[650px]'>
-                            {certificateUrl}
+                          <div
+                            id="certificate-block"
+                            className="relative flex flex-col items-center bg-[#FFF] bg-[url(/assets/AdoptionCertificate/Signed_Adoption_Certificate.png)] bg-cover bg-center w-[1020px] h-[650px]"
+                          >
+                            <label className="absolute top-73 text-4xl font-bold">
+                              {selectedAdoptee.cat_name}
+                            </label>
+                            <label className="absolute top-94 right-40 text-xl font-bold">
+                              {selectedAdoptee.adopter}
+                            </label>
+                            <label className="absolute top-102 right-140 text-xl font-bold">
+                              {selectedAdoptee.adoption_date}
+                            </label>
                           </div>
                         )}
 
